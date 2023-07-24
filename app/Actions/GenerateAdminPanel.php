54d8a7ad9f10ec88171f7e2124429332312afd1e
ismaelCalimaDev <santiago@calimasolutions.com>
2023-07-24 16:09:32 +0100

    wip

---
+++ app/Actions/GenerateAdminPanel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Actions;
+
+
+use App\Models\AuctionPools;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class GenerateAdminPanel
+{
+    use AsAction;
+
+    public string $commandSignature = 'generate:filamentize';
+
+    public function handle()
+    {
+        $tables = Schema::getAllTables();
+        $database = config('database.connections.mysql.database');
+        $tableReference = 'Tables_in_'.config('database.connections.mysql.database');
+        $directory = app_path("Models");
+
+        foreach ($tables as $table) {
+            $tableName = $table->$tableReference;
+            if (!File::exists($directory)) {
+                File::makeDirectory($directory);
+            }
+            $fileName = Str::ucfirst(Str::camel($tableName));
+            File::put($directory.'\\'. $fileName.'.php', "<?php\nnamespace App\Models;\nuse Illuminate\Database\Eloquent\Factories\HasFactory;\nuse Illuminate\Database\Eloquent\Model;\nuse Illuminate\Database\Eloquent\Relations\BelongsToMany;\nclass $fileName extends Model\n{\n\tprotected \$guarded = [];\n}");
+            Artisan::call("make:filament-resource $fileName");
+        }
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -42,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return $this->role === 'admin';
+    }
 }
